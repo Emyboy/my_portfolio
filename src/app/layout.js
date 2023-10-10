57b6fcee36f3a75ee787956b4e7027cd414fa5c1
Emy Ifeora <emyboybeats@gmail.com>
2023-10-10 22:16:32 +0100

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,6 +19,7 @@ export default function RootLayout({ children }) {
 					rel="stylesheet"
 					href="https://cdn.jsdelivr.net/gh/iconoir-icons/iconoir@main/css/iconoir.css"
 				/>
+				<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"></link>
 			</head>
 			<body
 				data-aos-easing="ease"
